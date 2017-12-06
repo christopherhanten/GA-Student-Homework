891f2ecdcd11c8629a203eecde18ddebba08060a
Your Name <controlsltrapidfire@gmail.comm>
2017-12-06 00:46:56 -0600

    Commit 9 - Created loops.js file

---
+++ Week_One/loops.js
@@ -0,0 +1,24 @@
+for (let i=0; i < 100; i++) {
+	console.log(i);
+}
+for (let t=99; t >= 0; t--) {
+  	console.log(t);
+}
+for (let r=0; r < 99; r++) {
+    if(r % 2 == 0)
+  	console.log(r);
+}
+for (let x=99; x >= 0; x--) {
+    if(x % 2 == 0)
+  	console.log(x);
+}
+for (let l=49; l <= 72; l++) {
+    console.log(l);
+}
+for (let f=81; f >= 26; f--) {
+    console.log(f);
+}
+for (let o=3; o <= 90; o++) {
+    if(o % 3 == 0)
+  	console.log(o);
+}
