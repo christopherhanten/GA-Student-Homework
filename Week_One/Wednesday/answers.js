07322338de34d83aad53f84df199fe6b9986ccd0
Your Name <controlsltrapidfire@gmail.comm>
2017-12-06 20:49:08 -0600

    Commit 3 - Excited Kittens answered

--- Week_One/Wednesday/answers.js
+++ Week_One/Wednesday/answers.js
@@ -1,9 +1,36 @@
+//Easy Going
 /*
 for (let i=1; i <= 20; i++) {
   console.log(i);
-}
+  }
 */
-for (let w = 0; w <=200; w++){
+//Get Even
+/*for (let w = 0; w <=200; w++){
   if (w % 2 == 0)
     console.log(w);
   }
+*/
+//Excited Kitten
+/*
+for (let kitteh = 1; kitteh <= 20; kitteh++) {
+  console.log("Love me, pet me! HSSSSSS!");
+}
+*/
+//I gave this one about 40 minutes. I cannot figure it out!
+/*
+function Math.random();
+var sayings = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+var randomSayings = sayings[Math.floor(Math.random() (sayings[1] [2] [3]))];
+for (let kat = 1; kat <= 20; kat++){
+  if (kat % 2 == 0) //Math.random
+  console.log (randomSayings);
+}
+*/
+//Getting to Know you
+//1.
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
+
+thom[0] = "Gameboy";
